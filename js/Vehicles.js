0f639a58e9942fda408ebf8689b48c68fafe4da6
Cory Lewis <cjl9703@yahoo.com>
2017-04-05 22:50:10 -0700

    Validation Completed?
    I added string validation for the form...  the only glitch I came across
    is if you click ouside the modal then it will dismiss but not clear the
    inputs.... not sure how to fix yet

--- js/Vehicles.js
+++ js/Vehicles.js
@@ -9,9 +9,6 @@ $(document).ready(function(){
   var Year = new Date().getFullYear();//Returns the year, Ex| 2017
   //alert(Month + "/"+ Day + "/" + Year);
 
-
-
-
   //====================== Click Events =======================================>
 
   ///Add Button Clicked
@@ -22,26 +19,83 @@ $(document).ready(function(){
 
   });
 
-  //====================== End Click Events ===================================>
+  $("#Modal_Cancel").click(function(){
+    ResetForm();
+  });
 
+  //====================== End Click Events ===================================>
 
 
   // ========================= Form Validation ================================>
 
     $("#Modal_Name_Input").keyup(function(){
-
-      //If naming scheme is correct... then do this..
-      if (validate_Naming_Scheme("Modal_Name_Input")) {
-        //Change from red x to green check
-        $("#Modal_Name_Validation").removeClass( "glyphicon-remove-circle" ).addClass( "glyphicon-ok-circle" );
-      }else {
-        //Change from green check to red x
-        $("#Modal_Name_Validation").removeClass( "glyphicon-ok-circle" ).addClass( "glyphicon-remove-circle" );
-      }
-
+        //Modal Name Validation
+        if (validate_Naming_Scheme("Modal_Name_Input")) {
+          //Change from red x to green check
+          $("#Modal_Name_Validation").removeClass( "glyphicon-remove-circle" ).addClass( "glyphicon-ok-circle" );
+        }else {
+          //Change from green check to red x
+          $("#Modal_Name_Validation").removeClass( "glyphicon-ok-circle" ).addClass( "glyphicon-remove-circle" );
+        }
     });
 
-  //=====> Validation Functions <==================\\
+    //Modal Department Validation
+    $("#Modal_Dept_Input").keyup(function(){
+        //If naming scheme is correct... then do this..
+        if (validate_Naming_Scheme("Modal_Dept_Input")) {
+          //Change from red x to green check
+          $("#Modal_Dept_Validation").removeClass( "glyphicon-remove-circle" ).addClass( "glyphicon-ok-circle" );
+        }else {
+          //Change from green check to red x
+          $("#Modal_Dept_Validation").removeClass( "glyphicon-ok-circle" ).addClass( "glyphicon-remove-circle" );
+        }
+    });
+
+    //Vehicle # Input (Block all non-numbers)
+    $('#Modal_Vehicle_Input').on('input', function (event) {
+        this.value = this.value.replace(/[^0-9]/g, '');
+        if (document.getElementById("Modal_Vehicle_Input").value.length == 2) {
+            //next focus
+            $("#Modal_Letter_Input").focus();
+            //Green Check
+            $("#Modal_Vehicle_Validation").removeClass( "glyphicon-remove-circle" ).addClass( "glyphicon-ok-circle" );
+        }else {
+            //Change from green check to red x
+            $("#Modal_Vehicle_Validation").removeClass( "glyphicon-ok-circle" ).addClass( "glyphicon-remove-circle" );
+        }
+    });
+
+    //Key Letter Validation
+    $("#Modal_Letter_Input").on("input", (function(){
+        //Block all non A-Z values
+        this.value = this.value.replace(/[^a-zA-Z]/, '');
+        //Force to uppercase
+        this.value = this.value.toUpperCase();
+        //Check for input and determine good or bad
+        if (document.getElementById("Modal_Letter_Input").value.length == 1) {
+            //focus next
+            $("#Modal_Destination_Input").focus();
+            //Green check
+            $("#Modal_Letter_Validation").removeClass( "glyphicon-remove-circle" ).addClass( "glyphicon-ok-circle" );
+        }else {
+            //red x
+            $("#Modal_Letter_Validation").removeClass( "glyphicon-ok-circle" ).addClass( "glyphicon-remove-circle" );
+        }
+    }));
+
+    $("#Modal_Destination_Input").keydown(function(){
+
+      //If naming scheme is correct... then do this..
+      if (validate_Naming_Scheme("Modal_Destination_Input")) {
+        //Change from red x to green check
+        $("#Modal_Destination_Validation").removeClass( "glyphicon-remove-circle" ).addClass( "glyphicon-ok-circle" );
+      }else {
+        //Change from green check to red x
+        $("#Modal_Destination_Validation").removeClass( "glyphicon-ok-circle" ).addClass( "glyphicon-remove-circle" );
+      }
+    });
+
+  //=====> Functions <==================\\
   //Pass in element Id to make function work properly
   function validate_Naming_Scheme(temp){
   //  var re = /^[A-Za-z]+$/;  <== This ONLY Allows Letters
@@ -55,7 +109,26 @@ $(document).ready(function(){
        }
 }// End validate_Naming_Scheme
 
-  //=====> End Validation Functions <===============\\
+  //Reset all inputs in form
+  function ResetForm(){
+    //Name
+    $("#Modal_Name_Input").val("");
+    $("#Modal_Name_Validation").removeClass( "glyphicon-ok-circle" ).addClass( "glyphicon-remove-circle" );
+    //Dept
+    $("#Modal_Dept_Input").val("");
+    $("#Modal_Dept_Validation").removeClass( "glyphicon-ok-circle" ).addClass( "glyphicon-remove-circle" );
+    //Vehicle
+    $("#Modal_Vehicle_Input").val("");
+    $("#Modal_Vehicle_Validation").removeClass( "glyphicon-ok-circle" ).addClass( "glyphicon-remove-circle" );
+    //Letter
+    $("#Modal_Letter_Input").val("");
+    $("#Modal_Letter_Validation").removeClass( "glyphicon-ok-circle" ).addClass( "glyphicon-remove-circle" );
+    //Destination
+    $("#Modal_Destination_Input").val("");
+    $("#Modal_Destination_Validation").removeClass( "glyphicon-ok-circle" ).addClass( "glyphicon-remove-circle" );
+  };
+
+  //=====> End Functions <===============\\
 
   // ========================= End Form Validation ============================>
 
